5b7cb79e6814a2defad3333de325b8a80f8440c2
ARPIT GUPTA <arpitgupta205001@gmail.com>
2026-05-10 02:47:44 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -2,21 +2,23 @@
 // server/server.js
 // ==========================================
 
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
 const projectRoutes = require("./routes/projectRoutes");
 const taskRoutes = require("./routes/taskRoutes");
-const dashboardRoutes = require(
-  "./routes/dashboardRoutes"
-);
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
+// Middleware
 app.use(express.json());
+
 app.use(
   cors({
     origin: "*",
@@ -24,23 +26,35 @@ app.use(
   })
 );
 
-mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 30000,
-})
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 30000,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
-app.use("/api/dashboard",
-  dashboardRoutes
-);
+app.use("/api/dashboard", dashboardRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API Running");
+// Serve Frontend
+const __dirname1 = path.resolve();
+
+app.use(express.static(path.join(__dirname1, "client", "dist")));
+
+// React Routes
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname1, "client", "dist", "index.html")
+  );
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server Running On ${process.env.PORT}`);
+// Server Start
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server Running On ${PORT}`);
 });
